6407d8a695b8738b9c4d869a3c64980aed566aae
elkafrawyel <mahmoud.ashraf.elkafrawy@gmail.com>
2020-01-07 11:44:20 +0200

    add chat room views

--- app/src/main/java/com/elwaha/bubbles/utilies/Constants.kt
+++ app/src/main/java/com/elwaha/bubbles/utilies/Constants.kt
@@ -1,8 +1,8 @@
 package com.elwaha.bubbles.utilies
 
 object Constants {
-    const val BASE_URL = "http://stbraq4it.com/rwaq/api/v1/"
-    const val IMAGES_BASE_URL = "http://stbraq4it.com/rwaq/public/uploads/"
+    const val BASE_URL = ""
+    const val IMAGES_BASE_URL = ""
     const val AUTHORIZATION_START = "Bearer"
     const val MAPVIEW_BUNDLE_KEY = "MapViewBundleKey"
 
